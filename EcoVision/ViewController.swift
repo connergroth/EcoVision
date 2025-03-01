127848a1004d0775ee5e61f0d8e1e0146b6876b2
Yusuf Morsy <yumo2500@colorado.edu>
2025-03-01 10:24:47 -0700

    Initial Commit

---
+++ EcoVision/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EcoVision
+//
+//  Created by Yusuf Morsy on 3/1/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ EcoVisionTests/EcoVisionTests.swift
@@ -0,0 +1,17 @@
+//
+//  EcoVisionTests.swift
+//  EcoVisionTests
+//
+//  Created by Yusuf Morsy on 3/1/25.
+//
+
+import Testing
+@testable import EcoVision
+
+struct EcoVisionTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
