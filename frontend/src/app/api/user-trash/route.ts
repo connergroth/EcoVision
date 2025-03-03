365cfd73025802a179295940b5b6309088411dff
Conner Groth <connergroth@gmail.com>
2025-03-03 12:46:06 -0700

    fixed leaderboard

--- frontend/src/app/api/user-trash/route.ts
+++ frontend/src/app/api/user-trash/route.ts
@@ -1,9 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 import { adminDb } from "@/firebase/firebaseAdminConfig";
 
-
-
-
 export async function GET(request: NextRequest) {
     console.log('hello');
     const { searchParams } = new URL(request.url);
